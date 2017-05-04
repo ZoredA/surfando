116f3fb63d2f53218e2499abe9832b60a27a12d6
ZORED <zbahmer@uwaterloo.ca>
2017-05-03 23:19:50 -0400

    some stuff

--- surfando.js
+++ surfando.js
@@ -8,9 +8,29 @@ function onGot(item) {
     color = item.color;
   }
   console.log("coloring");
+  if (window.location.href.includes('localhost')){
+    color = "red";
+  }
   document.body.style.border = "10px solid " + color;
+
 }
+
 console.log("Script loading");
 var getting = browser.storage.local.get("color");
 console.log(getting);
 getting.then(onGot, onError);
+
+// var site = browser.storage.local.get("url");
+// getting.then(function(item){
+  // var call_url = "http://localhost:8888/login";
+  // if (item.url){
+    // call_url = item.url;
+  // }
+  // if (window.location.href.includes(call_url)){
+    // var sending = browser.runtime.sendMessage({
+      // 'action':'site',
+      // 'url':window.location.href
+    // });
+  // }
+// },
+// (err) => {console.log(err);});
