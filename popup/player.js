eb4a5df521718fc6e1280beb66d6e961f2ee28f9
ZORED <zbahmer@uwaterloo.ca>
2017-05-05 18:45:50 -0400

    it is basically completely functional just not pretty

--- popup/player.js
+++ popup/player.js
@@ -36,6 +36,14 @@ document.addEventListener("click", (e) => {
   if (e.target.classList.contains("surfando")) {
     console.log('surfs up');
     console.log(e.target.name);
+    if (e.target.name === 'test'){
+      var src="https://upload.wikimedia.org/wikipedia/commons/e/eb/Beethoven_Moonlight_1st_movement.ogg"
+      var au = document.getElementById('player');
+      au.src = src;
+      au.load();
+      au.play();
+      return;
+    }
     var sending = browser.runtime.sendMessage({'action':e.target.name});
     sending.then( function(response){
       console.log('sent');
